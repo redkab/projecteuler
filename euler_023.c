e9fda9fbcedd0c24ea63df98c15d0de073dbb27c
Saketh Indurthi <saketh.indurthi@gmail.com>
2025-12-22 05:35:53 -0500

    update euler_023.c

--- euler_023.c
+++ euler_023.c
@@ -2,7 +2,7 @@
 int  is_abundant(int x)
 {
     int i, sum=0;
-    for(i=1; i<x; i++)
+    for(i=1; i<=x/2; i++)
     {
         if(x%i==0)sum += i;
     }
@@ -12,18 +12,21 @@ int  is_abundant(int x)
 }
 int main()
 {
-    int i, j, max;
+    int i, j, flag;
     long long sum=0;
     for(i=1; i<=28123; i++)
     {
+        flag=0;
         for(j=1; j<=i/2; j++)
         {
             if(is_abundant(j) && is_abundant(i-j))
             {
-                sum += i;
+                flag=1;
                 printf("i is %d j is %d sum is %lld\n", i, j, sum);
                 break;
             }
         }
+        if(flag==0) sum+= i;
     }
+    printf("Sum is %lld\n", sum);
 }
